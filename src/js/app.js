10531c3b71fd03fef5452fe1a7c3b65434d0fc94
jackylu1012 <jacky1352@gmail.com>
2018-11-25 14:30:56 +0800

    menu & hamburger build

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,10 @@
 import css from '../scss/main.scss';
 import slider from "./slider.js";
+import menu from "./menu.js";
 
 $(function(){
     // banner
     slider();
+    // menu
+    menu();
 })
---
+++ src/js/menu.js
@@ -0,0 +1,5 @@
+export default function slider(){
+    $('.menu-wrapper').on('click', function() {
+		$('.hamburger-menu').toggleClass('animate');
+	})
+}
